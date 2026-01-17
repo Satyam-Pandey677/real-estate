a50a0ffa0079075189c7d8aa4b0169edb71efa9d
Satyam-Pandey677 <satyampandey0241@gmail.com>
2026-01-17 10:44:05 +0530

    Refactor code structure for improved readability and maintainability

--- app/(nondashboard)/landing/HeroSection.tsx
+++ app/(nondashboard)/landing/HeroSection.tsx
@@ -1,5 +1,11 @@
+"use client"
+
 import Image from 'next/image'
 import React from 'react'
+import {motion} from "framer-motion"
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { Search } from 'lucide-react'
 
 const HeroSection = () => {
   return (
@@ -12,7 +18,40 @@ const HeroSection = () => {
             priority
         />
 
-        <div className='absolute inset-0 bg-black bg-opacity-60'></div>
+        <div className='absolute inset-0 bg-black/60'></div>
+        <motion.div
+          initial={{opacity:0, y:20}}
+          animate ={{opacity:1,y:0}}
+          transition={{duration:0.8}}
+          className='absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center w-full'
+        > 
+          <div className='max-w-4xl mx-auto px-16 sm:px-12'>
+            <h1 className='text-5xl font-bold text-white mb-4 '>
+                Start you journey to finding the perfect home
+            </h1>
+            <p className='text-xl text-white mb-8'>
+              Explore our wide range of properties tailored to fit your lifestyle and needs !
+            </p>
+
+            <div className='flex justify-center'>
+              <Input
+                type='text'
+                value="search query "
+                onChange={() => {}}
+                placeholder='Search by city, neighborhood or address'
+                className='w-full m-w-lg rounded-none rounded-l-xl border-none bg-white h-12' 
+              />  
+              <Button
+                  onClick={() => {}}
+                  className='bg-red-500 text-white rounded-none rounded-r-xl border-none hover:bg-red-600 h-12 font-bold'
+              >
+                Search
+              </Button>
+
+            </div>
+          </div>
+
+        </motion.div>
     </div>
   )
 }
