a7b0cbf4d55291b85ff50c18526a07107568e774
Satyam-Pandey677 <satyampandey0241@gmail.com>
2026-01-21 23:14:28 +0530

    add FooterSection component and enhance CallToActionSection with buttons

--- app/(nondashboard)/landing/CallToActionSection.tsx
+++ app/(nondashboard)/landing/CallToActionSection.tsx
@@ -2,6 +2,8 @@
 
 import Image from 'next/image'
 import {motion} from "framer-motion"
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 
 const CallToActionSection = () => {
   return (
@@ -20,11 +22,11 @@ const CallToActionSection = () => {
             transition={{duration:0.5}}
             whileInView={{opacity:1, y:0}}
             viewport={{once:true}}
-            className='relative max-w-41 xl:max-w-6xl mx-auto px-6 sm:px-12 xl:px-16 py-12'
+            className='relative max-w-4xl xl:max-w-6xl mx-auto px-6 sm:px-12 xl:px-16 py-12'
         >
             <div className='flex flex-col md:flex-row justify-between items-center '>
                 <div className='mb-6 md:mb-0 md:mr-10'>
-                    <h2 className='text-2xl font-bold text-while '>
+                    <h2 className='text-2xl font-bold text-white '>
                         Find Your Dream Rental Property  
                     </h2>
                 </div>
@@ -32,6 +34,22 @@ const CallToActionSection = () => {
                     <p className='text-white mb-3 '>
                         Discover a wide range of rental properties in your desired location.
                     </p>
+                    <div className='flex justify-center md:justify-start gap-4'>
+                        <button
+                            onClick={() => window.scrollTo({top:0, behavior:"smooth"})}
+                            className='inline-block text-gray-700 font-bold hover:bg-gray-2 px-6 py-3 bg-white rounded-lg'
+                        >
+                            Search
+                        </button>
+
+                        <Link
+                            href="/signup"
+                            className='inline-block text-white bg-red-500 rounded-lg px-6 py-3 font-semibold hover:bg-red-600'
+                            scroll={false}
+                        >
+                            Sign Up
+                        </Link>
+                    </div>
                 </div>
             </div>
         </motion.div>
